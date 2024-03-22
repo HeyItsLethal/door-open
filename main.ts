4b6520c9306343f0c1cbfbcdbe4c8e41e8eaa48c
Adam G <115652287+HeyItsLethal@users.noreply.github.com>
2024-03-22 13:23:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Hi")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "A") {
+        servos.P0.setAngle(90)
+    } else if (receivedString == "B") {
+        servos.P0.setAngle(0)
+    }
+})
+radio.setGroup(199)
+basic.forever(function () {
+	
+})
