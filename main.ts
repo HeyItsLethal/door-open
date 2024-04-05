3fe804fa3145d7d71251cb938d17696d647acb6e
Adam G <115652287+HeyItsLethal@users.noreply.github.com>
2024-04-05 12:50:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "A") {
         servos.P0.setAngle(90)
+        radio.sendString("C")
     } else if (receivedString == "B") {
         servos.P0.setAngle(0)
     }
